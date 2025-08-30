41554cf4791fbe02c5122dc475e7b45e6c0ba195
noxterr <mirko.dalfarra@gmail.com>
2025-08-30 10:42:09 +0200

    Wal and configuration
    Created the structure for adding a configuration TOML file and parsing it. Moreover, added the related WAL structure with the recovery process, a clearing process and a write-ahead process before writing operations to the cache

--- cmd/start.go
+++ cmd/start.go
@@ -1,30 +1,55 @@
-package cmd
+package main
 
 import (
-	"github.com/spf13/cobra"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 
 	"github.com/dalfrom/simplecache/pkg/tcp"
 )
 
-// StartCmd represents the start command
-var StartCmd = &cobra.Command{
-	Use:   "start",
-	Short: "Start the simplecache server",
-	Run: func(cmd *cobra.Command, args []string) {
-		ServerCache = &tcp.ServerCache{Port: port}
-		ServerCache.Start()
-	},
-}
+const defaultPort int = 4000
+const defaultConfigPath string = "/etc/simplecache/simplecache.toml"
 
-var StatusCmd = &cobra.Command{
-	Use:   "status",
-	Short: "Check the status of the simplecache server",
-	Run: func(cmd *cobra.Command, args []string) {
-		println("Checking simplecache server status...")
-	},
-}
+var (
+	port              int
+	configurationPath string
+)
 
-func init() {
-	// Add flags to the start command
-	StartCmd.Flags().IntVar(&port, "p", 4000, "Server port [defaults to 4000]")
+func main() {
+	// There are no provided config and port. Using the default ones
+	if len(os.Args) < 2 {
+		configurationPath = defaultConfigPath
+		port = defaultPort
+	} else if len(os.Args) == 2 {
+		// There is only the config provided. Using the default port
+		configurationPath = os.Args[1]
+		port = defaultPort
+	} else if len(os.Args) == 3 {
+		// Both config and port are provided
+		configurationPath = os.Args[1]
+
+		iPort, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println("Invalid port number. Using default port.")
+			port = defaultPort
+		} else {
+			port = iPort
+		}
+	}
+
+	serverCache := &tcp.ServerCache{Port: port}
+	serverCache.Start(configurationPath)
+
+	// Handle graceful shutdown
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		fmt.Println("Shutting down gracefully…")
+		serverCache.Stop(false)
+		os.Exit(0)
+	}()
 }
